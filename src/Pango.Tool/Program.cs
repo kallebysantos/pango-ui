38217ab56e5382abe1921c04237565abea72caed
kallebysantos <kalleby_santos@hotmail.com>
2024-02-20 00:42:44 +0000

    📦 NEW: Adding Pongo logo

--- src/Pango.Tool/Program.cs
+++ src/Pango.Tool/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Help;
+using System.CommandLine.Parsing;
+using Spectre.Console;
+
+var rootCommand = new RootCommand(
+    description: "Capture ants... I mean, components to your project"
+);
+
+var parser = new CommandLineBuilder(rootCommand)
+    .UseDefaults()
+    .UseHelp(ctx =>
+    {
+        ctx.HelpBuilder.CustomizeLayout(
+                _ =>
+                    HelpBuilder.Default
+                        .GetLayout()
+                        .Skip(1) // Skip the default command description section.
+                        .Prepend(_ => AnsiConsole.Write(@"
+                                             _/\/\/\/\/\/\-_
+ ____                                     _-//\/\/\/\/\/\/\/\>
+|  _ \    __ _   _ __     __ _    ___    /  \/\/\/\/\/\/\/\/\\>
+| |_) |  / _` | | '_ \   / _` |  / _ \  / .  \/\/\/\/\/\/\/\/\\>
+|  __/  | (_| | | | | | | (_| | | (_) | |    __--\__--/\/\/\/\/>
+|_|      \__,_| |_| |_|  \__, |  \___/  \__//  \| |\_____/   //
+                         |___/                 (_(_\________//"))
+                    );
+    })
+    .Build();
+
+await parser.Parse(args).InvokeAsync();
