e9bbeea87cba8a1a68e5fe9140ac51e48c1168d5
kallebysantos <kalleby_santos@hotmail.com>
2025-03-20 11:55:30 +0000

    chore: tailwindcss setup

--- src/Pango.UI/Program.cs
+++ src/Pango.UI/Program.cs
@@ -3,8 +3,7 @@ using Pango.UI.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -21,7 +20,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
