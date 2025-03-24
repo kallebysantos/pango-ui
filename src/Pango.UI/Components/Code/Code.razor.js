da5d953fcfc36b5719d40bc2947ec22f2e9699d9
kallebysantos <kalleby_santos@hotmail.com>
2025-03-24 14:36:29 +0000

    feat: add code component

---
+++ src/Pango.UI/Components/Code/Code.razor.js
@@ -0,0 +1,21 @@
+import { codeToHtml } from 'https://esm.sh/shiki@3.0.0'
+
+export async function ApplySyntaxHighlight(elementRef) {
+  elementRef.toggleAttribute("data-loading", true);
+  const codeLang = elementRef.getAttribute("data-language");
+  const codeStr = elementRef.querySelector("textarea").value;
+  const outputContainer = elementRef.querySelector("div#pango-ui-code-container");
+
+  const dark = await codeToHtml(codeStr, {
+    lang: codeLang,
+    theme: 'github-dark-default'
+  });
+
+  const light = await codeToHtml(codeStr, {
+    lang: codeLang,
+    theme: 'github-light-default'
+  });
+
+  outputContainer.innerHTML = dark + light;
+  elementRef.toggleAttribute("data-loading", false);
+}
