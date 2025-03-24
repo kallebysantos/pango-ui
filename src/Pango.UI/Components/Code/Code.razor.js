c71f33851ced4358230f654bec9cd4d803235da7
kallebysantos <kalleby_santos@hotmail.com>
2025-03-24 18:07:55 +0000

    stamp: code component with selectable text
    - Adding a textarea overlay on top of the code block

--- src/Pango.UI/Components/Code/Code.razor.js
+++ src/Pango.UI/Components/Code/Code.razor.js
@@ -3,20 +3,31 @@ import { codeToHtml } from 'https://esm.sh/shiki@3.0.0'
 export async function ApplySyntaxHighlight(elementRef) {
   elementRef.toggleAttribute("data-loading", true);
 
+  const codeValueWrapper = elementRef.querySelector("textarea");
+
   const codeLang = elementRef.getAttribute("data-language");
-  const codeStr = elementRef.querySelector("textarea").value;
   const outputContainer = elementRef.querySelector("div#pango-ui-code-container");
 
-  const dark = await codeToHtml(codeStr, {
+  const dark = await codeToHtml(codeValueWrapper.value, {
     lang: codeLang,
     theme: 'github-dark-default'
   });
 
-  const light = await codeToHtml(codeStr, {
+  const light = await codeToHtml(codeValueWrapper.value, {
     lang: codeLang,
     theme: 'github-light-default'
   });
 
   outputContainer.innerHTML = dark + light;
+
+  Array.from(outputContainer.querySelectorAll("pre"))
+    .forEach(codeBlock => {
+      codeValueWrapper.addEventListener('scroll', () => {
+        codeBlock.scrollTop = codeValueWrapper.scrollTop;
+        codeBlock.scrollLeft = codeValueWrapper.scrollLeft;
+      });
+    });
+
+
   elementRef.toggleAttribute("data-loading", false);
 }
