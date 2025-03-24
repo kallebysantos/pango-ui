3c837201255dc87603236c528d5301f99a6367ce
kallebysantos <kalleby_santos@hotmail.com>
2025-03-24 15:55:15 +0000

    stamp: add Pango.UI js module

--- src/Pango.UI/Components/PageScript/PageScript.razor.js
+++ src/Pango.UI/Components/PageScript/PageScript.razor.js
@@ -3,84 +3,85 @@
 const pageScriptInfoBySrc = new Map();
 
 function registerPageScriptElement(src) {
-    if (!src) {
-        throw new Error('Must provide a non-empty value for the "src" attribute.');
-    }
+  if (!src) {
+    throw new Error('Must provide a non-empty value for the "src" attribute.');
+  }
 
-    let pageScriptInfo = pageScriptInfoBySrc.get(src);
+  let pageScriptInfo = pageScriptInfoBySrc.get(src);
 
-    if (pageScriptInfo) {
-        pageScriptInfo.referenceCount++;
-    } else {
-        pageScriptInfo = { referenceCount: 1, module: null };
-        pageScriptInfoBySrc.set(src, pageScriptInfo);
-        initializePageScriptModule(src, pageScriptInfo);
-    }
+  if (pageScriptInfo) {
+    pageScriptInfo.referenceCount++;
+  } else {
+    pageScriptInfo = { referenceCount: 1, module: null };
+    pageScriptInfoBySrc.set(src, pageScriptInfo);
+    initializePageScriptModule(src, pageScriptInfo);
+  }
 }
 
 function unregisterPageScriptElement(src) {
-    if (!src) {
-        return;
-    }
+  if (!src) {
+    return;
+  }
 
-    const pageScriptInfo = pageScriptInfoBySrc.get(src);
-    if (!pageScriptInfo) {
-        return;
-    }
+  const pageScriptInfo = pageScriptInfoBySrc.get(src);
+  if (!pageScriptInfo) {
+    return;
+  }
 
-    pageScriptInfo.referenceCount--;
+  pageScriptInfo.referenceCount--;
 }
 
 async function initializePageScriptModule(src, pageScriptInfo) {
-    if (src.startsWith("./")) {
-        src = new URL(src.substr(2), document.baseURI).toString();
-    }
+  if (src.startsWith("./")) {
+    src = new URL(src.substr(2), document.baseURI).toString();
+  }
 
-    const module = await import(src);
+  const module = await import(src);
 
-    if (pageScriptInfo.referenceCount <= 0) {
-        return;
-    }
+  if (pageScriptInfo.referenceCount <= 0) {
+    return;
+  }
 
-    pageScriptInfo.module = module;
-    module.onLoad?.();
-    module.onUpdate?.();
+  pageScriptInfo.module = module;
+  module.onLoad?.();
+  module.onUpdate?.();
 }
 
 function onEnhancedLoad() {
-    /*for (const [src, { module, referenceCount }] of pageScriptInfoBySrc) {
-        if (referenceCount <= 0) {
-            module?.onDispose?.();
-            pageScriptInfoBySrc.delete(src);
-        }
-    }*/
-    for (const { module } of pageScriptInfoBySrc.values()) {
-      module?.onDispose?.();
-    }
+  /*for (const [src, { module, referenceCount }] of pageScriptInfoBySrc) {
+      if (referenceCount <= 0) {
+          module?.onDispose?.();
+          pageScriptInfoBySrc.delete(src);
+      }
+  }*/
+  for (const { module } of pageScriptInfoBySrc.values()) {
+    module?.onDispose?.();
+  }
 
-    for (const { module } of pageScriptInfoBySrc.values()) {
-        module?.onUpdate?.();
-    }
+  for (const { module } of pageScriptInfoBySrc.values()) {
+    module?.onUpdate?.();
+  }
 }
 
 export function afterWebStarted(blazor) {
-    customElements.define('page-script', class extends HTMLElement {
-        static observedAttributes = ['src'];
+  customElements.define('page-script', class extends HTMLElement {
+    static observedAttributes = ['src'];
 
-        attributeChangedCallback(name, oldValue, newValue) {
-            if (name !== 'src') {
-                return;
-            }
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (name !== 'src') {
+        return;
+      }
 
-            this.src = newValue;
-            unregisterPageScriptElement(oldValue);
-            registerPageScriptElement(newValue);
-        }
+      this.src = newValue;
+      unregisterPageScriptElement(oldValue);
+      registerPageScriptElement(newValue);
+    }
 
-        disconnectedCallback() {
-            unregisterPageScriptElement(this.src);
-        }
-    });
+    disconnectedCallback() {
+      unregisterPageScriptElement(this.src);
+    }
+  });
 
-    blazor.addEventListener('enhancedload', onEnhancedLoad);
+  blazor.addEventListener('enhancedload', onEnhancedLoad);
 }
+
