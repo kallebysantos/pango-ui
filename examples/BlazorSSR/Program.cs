1c326148dd48315f99c26e79a46d3f3c9da08f2d
kallebysantos <kalleby_santos@hotmail.com>
2025-03-30 20:05:31 +0100

    stamp: serving static files from SSR project

--- examples/BlazorSSR/Program.cs
+++ examples/BlazorSSR/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
 using TailwindMerge.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +10,15 @@ builder
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddDirectoryBrowser();
+
 // Add TailwindMerge support
 builder.Services.AddTailwindMerge();
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.WebHost.GetSetting("urls") ?? string.Empty),
+});
 
 var app = builder.Build();
 
@@ -21,9 +30,37 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+// Alternatively, use AppDomain.CurrentDomain.BaseDirectory
+string baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot", "Docs");
+
+var fileProvider = new PhysicalFileProvider(baseDir);
+var requestPath = "/Docs";
+
+// Create content type provider and add .razor and .mdx mappings
+var contentTypeProvider = new FileExtensionContentTypeProvider();
+contentTypeProvider.Mappings[".razor"] = "text/plain"; // Serve .razor as plain text
+contentTypeProvider.Mappings[".mdx"] = "text/markdown"; // Serve .mdx as markdown
+
+// Enable displaying browser links.
+app.UseStaticFiles(
+    new StaticFileOptions
+    {
+        FileProvider = fileProvider,
+        RequestPath = requestPath,
+        ContentTypeProvider = contentTypeProvider,
+    }
+);
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDirectoryBrowser(
+        new DirectoryBrowserOptions { FileProvider = fileProvider, RequestPath = requestPath }
+    );
+}
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
+app.UseFileServer(enableDirectoryBrowsing: true);
+
 app.UseAntiforgery();
 
 app.MapRazorComponents<BlazorSSR.App>()
