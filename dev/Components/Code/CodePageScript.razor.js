31984c0392095a061af0e6aad6000b17189bd42a
kallebysantos <kalleby_santos@hotmail.com>
2025-05-05 00:41:16 +0100

    cleaning: removing old files

--- dev/Components/Code/CodePageScript.razor.js
+++
@@ -1,9 +0,0 @@
-import { ApplySyntaxHighlight } from './Code.razor.js';
-
-/**
- * Applies code syntax highligth on enhanced load
- */
-export function onUpdate() {
-  Array.from(document.querySelectorAll("#pango-ui-code"))
-    .forEach(ApplySyntaxHighlight);
-}
--- dev/Components/Tabs/TabsPageScript.razor.js
+++
@@ -1,9 +0,0 @@
-import { RegisterTabs } from './Tabs.razor.js';
-
-/**
- * Register tabs on enhanced load
- */
-export function onUpdate() {
-  Array.from(document.querySelectorAll("#pago-ui-tabs"))
-    .forEach(RegisterTabs);
-}
