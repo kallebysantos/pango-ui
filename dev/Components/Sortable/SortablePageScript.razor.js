d1ba140fc8f5e8ed87429803f1ebfe6d74de095b
kallebysantos <kallebysantos@users.noreply.github.com>
2025-05-04 23:24:28 +0000

    deploy: 132a42ef7ce9754d2fb5835e2a6c2099bd711556

--- dev/Components/Sortable/SortablePageScript.razor.js
+++
@@ -1,9 +0,0 @@
-import { ApplySortable } from "./Sortable.razor.js";
-
-/**
- * Applies sortable interactivity on enhanced load
- */
-export function onUpdate() {
-  Array.from(document.querySelectorAll("#pango-ui-sortable"))
-    .forEach(ApplySortable);
-}
