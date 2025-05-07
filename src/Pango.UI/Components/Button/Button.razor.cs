508afad8cc67c67c9fce22cab63dcec80c4867b1
kallebysantos <kalleby_santos@hotmail.com>
2025-05-07 15:13:56 +0100

    feat: removing UIComponent dependency

--- src/Pango.UI/Components/Button/Button.razor.cs
+++ src/Pango.UI/Components/Button/Button.razor.cs
@@ -1,9 +1,24 @@
 using Microsoft.AspNetCore.Components;
+using TailwindMerge;
 
 namespace Pango.UI.Components;
 
 public partial class Button
 {
+    /// <summary>
+    /// Gets or sets a collection of additional attributes that will be applied to the created element.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    [Inject]
+    protected TwMerge TwMerge { get; set; } = null!;
+
+    /// <summary>
+    /// Merge Tailwind CSS classes without style conflicts
+    /// </summary>
+    public string? Tw(params string?[] classNames) => TwMerge.Merge(classNames);
+
     public enum Sizes
     {
         Default,
