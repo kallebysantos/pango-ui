7900696edf0fcb4ad412ffdafff2cf5e5b9cb4cd
Carlos Saraiva <62428073+Saraivinha1703@users.noreply.github.com>
2026-01-08 11:38:35 +0000

    feat: add checkbox component and editorconfig file (#18)
    * 👌 IMPROVE: trying to keep the same formatting options accross IDEs
    
    * 📦 NEW: Adding checkbox components to select options
    
    * 🐛 FIX: peer was not working, it should  be with the label
    
    * 📦 NEW: Adding Checkbox mdx page, adding checkbox docs path in home and testing in development examples
    
    * 🐛 FIX: matching line height with checkbox
    
    * 🐛 FIX: Enabling value change and function on checked change
    
    * Fixing Checkbox component to work with EditForm
    
    This checkbox component inherits from Blazor's InputCheckbox and works
    with EditForm. NOTE: You must bind a value so the checkbox shows up
    
    * Fixing file format
    
    * Using examples
    
    * Making examples work with the new checkbox component binding values and using the EditForm

---
+++ src/Pango.UI/Components/Checkbox/Checkbox.razor.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using TailwindMerge;
+
+namespace Pango.UI.Components;
+
+public partial class Checkbox : InputCheckbox
+{
+    [Inject]
+    protected TwMerge TwMerge { get; set; } = null!;
+
+    /// <summary>
+    /// Merge Tailwind CSS classes without style conflicts
+    /// </summary>
+    private string? Tw(params string?[] classNames) => TwMerge.Merge(classNames);
+}
